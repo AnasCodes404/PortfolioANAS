5532ecbc4b955b0faa31ec5a02fa7f5eec4fdde6
Anas <tamimshaikh2011@gmail.com>
2024-07-15 16:41:30 +0300

    2nd commit

--- src/section/Hero/Hero.jsx
+++ src/section/Hero/Hero.jsx
@@ -45,19 +45,19 @@ function Hero(){
             </div>
             <div className={styles.info}>
                 <h1>ANAS <br/> SHAIKH</h1>
-                <h2>FrontEnd Developer</h2>
+                <h2>Software Engineer</h2>
                 <span>
-                    <a href="https://twitter.com" target='_blank'>
+                    <a href="https://x.com/anascodes404?s=21&t=Sy4IK1-UrFAEr4H4fwqAYw" target='_blank'>
                         <img src={twitterIcon} alt="twitter icon" />
                     </a>
-                    <a href="https://ggithub.com" target='_blank'>
+                    <a href="https://github.com/AnasCodes404" target='_blank'>
                         <img src={githubIcon} alt="githubIcon" />
                     </a>
-                    <a href="https://linkedin.com" target='_blank'>
+                    <a href="https://www.linkedin.com/in/anas-shaikh-03326b25a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target='_blank'>
                         <img src={linkedinIcon} alt="linkedinIcon icon" />
                     </a>
                 </span>
-                <p className={styles.description}>Designing modern web application from 2017   Extensively providing Ai solution and Data Modelling</p>
+                <p className={styles.description}>Designing modern web application from 20  Extensively providing Ai solution and Data Modelling</p>
                 <a href={CV}>
                     <button className='hover' download>
                         Resume
